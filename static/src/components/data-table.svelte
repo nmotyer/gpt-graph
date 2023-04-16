7cdbfd572750cf5899dcbf8678c9e1d4366de159
nmotyer <nmotyer@unimelb.edu.au>
2023-04-16 21:49:51 +1000

    cleaned up data table frontend made a basic shrink/expand

--- static/src/components/data-table.svelte
+++ static/src/components/data-table.svelte
@@ -1,37 +1,40 @@
 <script lang="ts">
     export let data: { [key: string]: any }[];
-    import { onMount } from 'svelte';
-    import { slide } from 'svelte/transition';
+    import { scale, slide } from 'svelte/transition';
 
-    let init = false;
-    onMount(() => {
-    init = true;
-  });
+    let isTableCollapsed = false;
+    let collapseIcon = '▲';
+
+  function toggleTable() {
+    isTableCollapsed = !isTableCollapsed;
+    collapseIcon = isTableCollapsed ? '▼' : '▲';
+  }
   </script>
   
   <table class="min-w-full divide-y divide-gray-200 mt-auto">
-    <thead class="bg-gray-50">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <thead class="bg-gray-50  cursor-pointer" on:click={toggleTable}>
       <tr>
         {#each Object.keys(data[0]) as key}
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-gray-200">
             {key}
           </th>
         {/each}
       </tr>
     </thead>
-    <tbody class="bg-white divide-y divide-gray-200">
+    {#if !isTableCollapsed}
+    <tbody class="bg-white divide-y divide-gray-200" transition:slide>
       {#each data as row}
-        {#if init}
-          <tr transition:slide|local={{axis: 'y', duration: 400}}>
+          <tr>
           {#each Object.values(row) as value}
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
               {value}
             </td>
           {/each}
         </tr>
-        {/if}
       {/each}
     </tbody>
+    {/if}
   </table>
   
   <style>
